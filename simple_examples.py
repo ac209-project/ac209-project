bf9c399d7c918fbd7efdbda5aa4ec65091fbc100
Nate <nathaniel.m.stein@gmail.com>
2017-11-16 14:45:36 -0500

    Added example to extract info on single user playlist.

--- simple_examples.py
+++ simple_examples.py
@@ -6,6 +6,8 @@ import pprint
 
 from utils import get_auth_spotipy
 
+section_break = lambda x:'\n{0}\n{1}\n{0}\n'.format('-'*30, x)
+
 sp = get_auth_spotipy()
 
 
@@ -23,4 +25,23 @@ def user_playlists ():
     for playlist in playlists['items']:
         print(playlist['name'])
 
-muse_search_results()
+
+def user_playlist ():
+    """Retrieving specific playlist info. Prints artists in playlist."""
+    user = 'indiefolkradio'
+    playlist_id = '1Qs40FRP061tVZ9x2npAE3'
+    playlist = sp.user_playlist(user, playlist_id)
+
+    for i, track in enumerate(playlist['tracks']['items']):
+        print(section_break('Track {}'.format(i + 1)))
+        print('Name: {}'.format(track['track']['name']))
+        print('Spotify ID: {}'.format(track['track']['id']))
+        print('Album: {}'.format(track['track']['album']['name']))
+
+        # Example of getting track information with the id provided.
+        # track_id = track['track']['id']
+        # track_info = sp.track(track_id)
+        # print(track_info['album']['name'])
+
+
+user_playlist()
